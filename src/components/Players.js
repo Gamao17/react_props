68dfefbd90554e7e55ebd1d3bb941fd5ed76e0f4
Mounir Ismael <ismaelmounir@gmail.com>
2023-10-17 01:36:39 +0000

    v1.1 react props

--- src/components/Players.js
+++ src/components/Players.js
@@ -1,4 +1,4 @@
- const PlayersTab = [{name : "Messi", team : "America", notionality : "Argentina", age : "36", picture : "https://library.sportingnews.com/2023-06/Messi_Miami_FTR_2.png"},
+ const PlayersTab = [{name : "Messi", team : "Inter-Miami", notionality : "Argentina", age : "36", picture : "https://library.sportingnews.com/2023-06/Messi_Miami_FTR_2.png"},
     {name : "Cristiano Ronaldo", team : "FC Al-Nassr", notionality : "Portugal", age : "38", picture : "https://static.mensup.fr/29/2023/10/photo_article/37819/18723/1200-L-cr7-redevient-le-footballeur-le-mieux-pay-au-monde-mbapp-sort-du-podium.webp"},
     {name : "Kilyan Mbapé", team : "PSG", notionality : "French", age :"24", picture : "https://images.rtl.fr/~c/770v513/rtl/www/1425545-kylian-mbappe-avec-les-bleus-au-stade-de-france-le-8-juin-2021.jpg"},
     {name : "Neymar Junior", team : "PSG", notionality : "Brasil", age :"31", picture :"https://media.cnn.com/api/v1/images/stellar/prod/230307102122-01-neymar-021423.jpg?c=original"},
